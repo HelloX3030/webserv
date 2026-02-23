92a178837fd8a2b8c3ea129e7dcb24202140c788
Ganesha O'Donnell <ganeshaodonnell@pm.me>
2026-02-23 15:11:14 +0100

    feat(config-prs): validate. make var name clearer (t -> tok (token))

--- src/classes/ConfigParser/ConfigParser_3e_interpret.cpp
+++ src/classes/ConfigParser/ConfigParser_3e_interpret.cpp
@@ -8,19 +8,19 @@
 grammar: host_port = port | host, ":", port ;
 colon presence distinguishes the 2 forms.
 C++17 if-init scopes pos to the branch where it is meaningful. */
-ListenAddress ConfigParser::parse_host_port(const Token& t)
+ListenAddress ConfigParser::parse_host_port(const Token& tok)
 {
     ListenAddress addr;
     addr.host = "0.0.0.0"; // default: bind to all interfaces
 
-    if (auto pos = t.value.find(':'); pos != std::string::npos)
+    if (auto pos = tok.value.find(':'); pos != std::string::npos)
     {
-        addr.host = t.value.substr(0, pos);
-        addr.port = parse_port(t.value.substr(pos + 1), t.line);
+        addr.host = tok.value.substr(0, pos);
+        addr.port = parse_port(tok.value.substr(pos + 1), tok.line);
     }
     else
     {
-        addr.port = parse_port(t.value, t.line);
+        addr.port = parse_port(tok.value, tok.line);
     }
     return addr;
 }
@@ -55,16 +55,16 @@ stoull over stoul: on 32-bit platforms size_t is 32 bits; stoull gives
 64-bit precision before the cast, catching overflow stoul would truncate.
 static_cast<unsigned char> on isdigit: char may be signed; passing a
 negative value to isdigit is undefined behaviour. */
-size_t ConfigParser::parse_size(const Token& t)
+size_t ConfigParser::parse_size(const Token& tok)
 {
-    const std::string& s = t.value;
+    const std::string& s = tok.value;
     size_t i = 0;
     while (i < s.size() && std::isdigit(static_cast<unsigned char>(s[i])))
         ++i;
 
     if (i == 0)
         throw std::runtime_error(
-            "[config] line " + std::to_string(t.line) +
+            "[config] line " + std::to_string(tok.line) +
             ": invalid size value '" + s + "'");
 
     unsigned long long n;
@@ -72,7 +72,7 @@ size_t ConfigParser::parse_size(const Token& t)
     catch (...)
     {
         throw std::runtime_error(
-            "[config] line " + std::to_string(t.line) +
+            "[config] line " + std::to_string(tok.line) +
             ": size value out of range '" + s + "'");
     }
 
@@ -91,6 +91,6 @@ size_t ConfigParser::parse_size(const Token& t)
     }
 
     throw std::runtime_error(
-        "[config] line " + std::to_string(t.line) +
+        "[config] line " + std::to_string(tok.line) +
         ": invalid size suffix in '" + s + "'");
 }
